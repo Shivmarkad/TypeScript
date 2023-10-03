d30dc36e344c317192d58a36d278fa0cc1585bfc
Shivganesh <shivmarkad99@gmail.com>
2023-10-03 16:56:37 +0530

    [Shivganesh] Added: interfaces in typescript to apply properties of it to object

---
+++ interfaces.js
@@ -0,0 +1,18 @@
+//type aliases
+//using custom type with custom name for variable
+var studentName = "Shiv";
+var studentAge = 24;
+var standardOfStudent = 12;
+//creating object with name parameter type
+var MyStudent = {
+    stdName: studentName,
+    stdAge: studentAge,
+    standard: standardOfStudent
+};
+//new object with interface properties
+var newCar = {
+    year: 2017,
+    name: "Tata",
+    owner: "Sachine",
+    color: "blue"
+};
---
+++ interfaces.ts
@@ -0,0 +1,48 @@
+//type aliases
+
+//types with custom names
+type name = string;
+type age = number;
+type standard = number;
+
+
+//custom type with use of types with custom names
+type newstudent = {
+    stdName:string,
+    stdAge:number,
+    standard:number
+}
+
+//using custom type with custom name for variable
+const studentName: name = "Shiv";
+const studentAge : age = 24;
+const standardOfStudent: standard = 12
+
+//creating object with name parameter type
+const MyStudent: newstudent = {
+    stdName:studentName,
+    stdAge:studentAge,
+    standard:standardOfStudent
+}
+
+//interfaces
+//only apply to object types
+
+interface CarDetail{
+    year: number,
+    name: string,
+    owner: string
+}
+
+//extending interfaces which will have same properties with additional new 
+interface coloredCar extends CarDetail{
+    color: string
+}
+
+//new object with interface properties
+const newCar: coloredCar = {
+    year:2017,
+    name:"Tata",
+    owner: "Sachine",
+    color: "blue"
+}
